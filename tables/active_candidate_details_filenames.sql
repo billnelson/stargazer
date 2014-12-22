14af909276bcd6dd69abe53aba7f4645516d96ed
Bill Nelson <bill.nelson@motionrp.com>
2014-12-22 12:37:09 -0500

    initial project version

---
+++ tables/active_candidate_details_filenames.sql
@@ -0,0 +1,22 @@
+USE [LibertyMutual]
+GO
+
+/****** Object:  Table [dbo].[active_candidate_details_filenames]    Script Date: 12/05/2014 14:39:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[active_candidate_details_filenames](
+	[filename] [varchar](256) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
---
+++ tables/daily_can_activity_log_filenames.sql
@@ -0,0 +1,22 @@
+USE [LibertyMutual]
+GO
+
+/****** Object:  Table [dbo].[daily_can_activity_log_filenames]    Script Date: 12/10/2014 14:39:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[daily_can_activity_log_filenames](
+	[filename] [varchar](256) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
